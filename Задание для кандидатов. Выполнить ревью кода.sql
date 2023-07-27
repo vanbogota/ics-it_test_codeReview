af9edea637ff0ad5ef76fd74bbf565b5db81e0b8
Ivan <84347281+vanbogota@users.noreply.github.com>
2023-07-27 21:22:00 +0300

    Добавил комментарии к коду с ошибками

--- Задание для кандидатов. Выполнить ревью кода.sql
+++ Задание для кандидатов. Выполнить ревью кода.sql
@@ -1,25 +1,43 @@
+/*
+	В начале скрипта объекта необходимо писать поясняющий комментарий;
+	Для того, чтобы повторное выполнение скрипта не приводило к возникновению
+ошибки, необходимо проверять существование в БД создаваемого объекта
+*/
 create procedure syn.usp_ImportFileCustomerSeasonal
 	@ID_Record int
 as
 set nocount on
 begin
 	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
-	declare @ErrorMessage varchar(max)
+	/* 
+		Для объявления переменных declare используется один раз. Дальнейшее
+	переменные перечисляются через запятую с новой строки, если явно не требуется
+	писать declare
+	*/
+	declare @ErrorMessage varchar(max) 
 
+-- Комментарий пишется непосредственно над строкой кода
 -- Проверка на корректность загрузки
 	if not exists (
+	-- В условных операторах весь блок смещается на 1 отступ
 	select 1
 	from syn.ImportFile as f
 	where f.ID = @ID_Record
 		and f.FlagLoaded = cast(1 as bit)
-	)
-		begin
+	)	
+	-- if и else с begin/end должны быть на одном уровне
+		begin 
 			set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
 
 			raiserror(@ErrorMessage, 3, 1)
-			return
+			-- пропущена пустая строка перед return. Пустыми строками отделяются разные логические блоки кода
+			return 
 		end
-
+	
+	/*
+		Ключевые слова, названия системных функций и все операторы пишутся со строчной буквы;
+		При создании объектов нужен пробел после названия объекта;
+	*/
 	CREATE TABLE #ProcessedRows(ActionType varchar(255), ID int)
 	
 	--Чтение из слоя временных данных
@@ -32,22 +50,28 @@ begin
 		,cd.ID as ID_dbo_CustomerDistributor
 		,cast(isnull(sa.FlagActive, 0) as bit) as FlagActive
 	into #CustomerSeasonal
+	-- Алиас задается с помощью ключевого слова as
 	from syn.SA_CustomerSeasonal cs
+		-- Все виды join должны указываться явно		
 		join dbo.Customer as cc on cc.UID_DS = sa.UID_DS_Customer
 			and cc.ID_mapping_DataSource = 1
 		join dbo.Season as s on s.Name = sa.Season
 		join dbo.Customer as cd on cd.UID_DS = sa.UID_DS_CustomerDistributor
 			and cd.ID_mapping_DataSource = 1
+		-- При соединение двух таблиц, сперва после on указываем поле присоединяемой таблицы
 		join syn.CustomerSystemType as cst on sa.CustomerSystemType = cst.Name
 	where try_cast(sa.DateBegin as date) is not null
 		and try_cast(sa.DateEnd as date) is not null
 		and try_cast(isnull(sa.FlagActive, 0) as bit) is not null
-
+	
+	-- Для комментариев в несколько строк используется конструкция /* */
 	-- Определяем некорректные записи
 	-- Добавляем причину, по которой запись считается некорректной
 	select
+		-- Дана ссылка на не обявленный алиас.
 		sa.*
 		,case
+			-- При написании конструкции с case, необходимо, чтобы when был под case с 1 отступом, then с 2 отступами			
 			when cc.ID is null then 'UID клиента отсутствует в справочнике "Клиент"'
 			when cd.ID is null then 'UID дистрибьютора отсутствует в справочнике "Клиент"'
 			when s.ID is null then 'Сезон отсутствует в справочнике "Сезон"'
@@ -58,6 +82,7 @@ begin
 		end as Reason
 	into #BadInsertedRows
 	from syn.SA_CustomerSeasonal as cs
+	-- Все виды join пишутся с 1 отступом
 	left join dbo.Customer as cc on cc.UID_DS = sa.UID_DS_Customer
 		and cc.ID_mapping_DataSource = 1
 	left join dbo.Customer as cd on cd.UID_DS = sa.UID_DS_CustomerDistributor and cd.ID_mapping_DataSource = 1
@@ -70,6 +95,7 @@ begin
 		or try_cast(sa.DateBegin as date) is null
 		or try_cast(sa.DateEnd as date) is null
 		or try_cast(isnull(sa.FlagActive, 0) as bit) is null
-
+-- Лишние пустые строки перед end
+	
 		
 end
